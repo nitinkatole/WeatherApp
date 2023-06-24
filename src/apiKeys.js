d5affad8bed3b394f308bf8d360af7bb518eb0a3
Nitin Ashok Katole <101620533+nitinkatole@users.noreply.github.com>
2023-06-24 09:17:13 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "bf08f5b33de742577760f8e7372f4622",
+  key: "Enter Your Api_Keys Here",
   base: "https://api.openweathermap.org/data/2.5/",
 };
